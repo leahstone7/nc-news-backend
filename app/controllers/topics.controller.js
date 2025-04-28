f935fa8fa95e86ba614d98c6cfe366f9dbe67b6e
Leah Stone <leahstone@MacBook-Pro-2.local>
2025-04-28 14:29:53 +0100

    this commit contains tests, updated api folder, new controller and model file for kata 3 get article id

--- app/controllers/topics.controller.js
+++ app/controllers/topics.controller.js
@@ -3,7 +3,6 @@ const { selectTopics } = require("../models/topics.model.js")
 exports.getTopics = (req, res, next) => {
     selectTopics()
     .then((result) => {
-        console.log(result, "<<<<<<<")
         res.status(200).send({topics: result})
     })
     .catch((err) => {
