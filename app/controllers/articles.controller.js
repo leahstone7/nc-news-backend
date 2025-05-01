aabcd88397cd7f18053c5bdd9e85738ce232c76f
Leah Stone <leahstone@MacBook-Pro-2.local>
2025-05-01 14:17:47 +0100

    task 10

--- app/controllers/articles.controller.js
+++ app/controllers/articles.controller.js
@@ -17,7 +17,8 @@ exports.getArticleId = (req, res, next) => {
 }
 
 exports.getArticles = (req, res, next) => {
-    selectArticles()
+    const {sort_by, order} = req.query
+    selectArticles(sort_by, order)
     .then((result) => {
         res.status(200).send({articles: result})
     })
--- app/models/articles.model.js
+++ app/models/articles.model.js
@@ -1,13 +1,25 @@
 const db = require("../../db/connection")
+const { sort } = require("../../db/data/test-data/articles")
 
-exports.selectArticles = () => {
-    return db
-    .query(`SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, 
-    COUNT (comments.comment_id) :: INT AS comment_count FROM articles
+exports.selectArticles = (sort_by = "created_at", order = "desc") => {
+    const allowedInputs = ["article_id", "author", "title", "topic", "created_at", "votes", "article_img_url"]
+    const allowedOrders = ["desc", "asc"]
+
+    if(!allowedInputs.includes(sort_by)){
+       sort_by = "created_at"
+       return Promise.reject({status: 400, msg: "Bad request"})
+    }
+
+    if(!allowedOrders.includes(order)){
+        order = "desc"
+    }
+
+    let queryStr = `SELECT articles.article_id, articles.author, articles.title, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT (comments.comment_id) :: INT AS comment_count FROM articles
     LEFT OUTER JOIN comments ON articles.article_id = comments.article_id
     GROUP BY articles.article_id
-    ORDER BY articles.created_at DESC
-    `)
+    ORDER BY ${sort_by} ${order}`
+    
+    return db.query(queryStr)
     .then((result) => {
         return result.rows
     })
